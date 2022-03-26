48d94b0d61f066e1e511c8b20cd8c8c625018ec0
Ons Hadriche <ounshadriche@gmail.com>
2022-03-26 22:28:27 +0100

    Set up user Schema

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    
+    lastName: {
+        type: String
+    },
+    birthDay:Number,
+    Genre: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
