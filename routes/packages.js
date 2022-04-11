030f3f7b9f41076326472855079ed874cdf231d9
Ons Hadriche <ounshadriche@gmail.com>
2022-04-12 01:34:04 +0200

    Setup package router

--- routes/packages.js
+++ routes/packages.js
@@ -1,13 +1,13 @@
-const express= ('express')
+const express = require('express')
 const router = express.Router()
-const { getAllPackages,getPackages, getOnePackage, createPackage } = require("../controllers/packages")
+const { getAllPackages,getPackages, getOnePackage, createPackage, updatePackage } = require("../controllers/packages")
 
 
 router.get('/', getAllPackages)
 router.get('/country', getPackages)
 router.get('/:id', getOnePackage)
 router.post('/', createPackage)
-router.put('/:id')
+router.put('/:id', updatePackage)
 
 module.exports = router
 
