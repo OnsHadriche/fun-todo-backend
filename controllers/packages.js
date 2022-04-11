73cff0e8bacf68899dd2799815f6681a6b19e66f
Ons Hadriche <ounshadriche@gmail.com>
2022-04-12 00:53:52 +0200

    Create package controller

--- controllers/packages.js
+++ controllers/packages.js
@@ -1,1 +1,85 @@
-const Package = require('../models/Package')
+const Package = require("../models/Package");
+const { packageValidator } = require("../utilities/validators");
+//get all packages 
+const getAllPackages = async (req, res) => {
+  try {
+    const package = await Package.find();
+    res.status.json({ package });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+//find packages by country
+const getPackages = async (req, res) => {
+    try {
+      const packages = await Package.findOne(req.body.country);
+      res.status.json({ packages });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+const getOnePackage = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const package = await Package.findById(id);
+    if (!package) {
+      return res.status(404).json({ error: "Package not found" });
+    }
+    res.status(200).json(package);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+const createPackage = async (req, res) => {
+  try {
+    const validationResult = packageValidator.validate(req.body, {
+      abortEarly: false,
+    });
+    if (validationResult.error) {
+      res.status(400).json(validationResult);
+    }
+    const { photo, title, price, details, country } = req.body;
+    await new Package({
+      photo: photo,
+      title: title,
+      price: price,
+      details: details,
+      country: country,
+    }).save();
+    res.status(201).json({ message: "Package created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+const updatePackage = async (req, res) => {
+  try {
+    const packageToUpdateId = req.params.id;
+    const validationResult = packageValidator.validate(req.body, {
+      abortEarly: false,
+    });
+    if (validationResult.error) {
+      return res.status(400).json(validationResult);
+    }
+    const package = await Package.findByIdAndUpdate(
+      packageToUpdateId,
+      req.body
+    );
+    if (!package) {
+      return res.status(404).json({ error: "Package not found" });
+    } else {
+      res
+        .status(201)
+        .json({ message: "Package uptdated successfully", package });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getAllPackages,
+  getPackages,
+  getOnePackage,
+  createPackage,
+  updatePackage
+};
