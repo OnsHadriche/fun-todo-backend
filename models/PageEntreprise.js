d9c55c252920a8476be33a8fbf1e1baee6ba1ee1
Ons Hadriche <ounshadriche@gmail.com>
2022-04-12 15:41:57 +0200

    Setup PageEntreprise schema

---
+++ models/PageEntreprise.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const pageSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  photo: {
+    type: String,
+    default:''
+  },
+  contact: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+module.exports = mongoose.model("PageEntreprise", pageSchema);
