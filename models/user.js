c5dca14fe06221a4d23b7c2d8e19f54cb0de7287
Ons Hadriche <ounshadriche@gmail.com>
2022-04-12 04:19:52 +0200

    Setup user model

--- models/user.js
+++ models/user.js
@@ -1,28 +1,31 @@
-
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    
-    lastName: {
-        type:String
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    phoneNumber : {
-        type: String
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  photo: {
+    type: String,
+    default:''
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
 
-module.exports = mongoose.model('User', userSchema)
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+ 
+});
+
+module.exports = mongoose.model("User", userSchema);
