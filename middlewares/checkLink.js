681f638f5ccfa2dc30ee34930c7536611d369ee8
Ons Hadriche <ounshadriche@gmail.com>
2022-04-17 16:26:43 +0200

    Set up check the link

--- middlewares/checkLink.js
+++ middlewares/checkLink.js
@@ -1,8 +1,12 @@
 const Token = require("../models/verificationToken")
+const jwt = require('jsonwebtoken')
 
 module.exports= async function(req, res, next){
     try {
      const {userId, token} = req.body
+     if(!token){
+       return res.status(401).json({error: "No token provided, authorization denied"})
+     }
      const verifiedLink = await Token.find({id : userId, token:token})
      if(!verifiedLink){
        return res.status(404).json({error: 'Invalid request/or token expired'})
