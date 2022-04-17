07c74eccab1d562637d5aa19e6d1cae668f881c9
Ons Hadriche <ounshadriche@gmail.com>
2022-04-17 16:47:18 +0200

    Add root to remove page  and admin

--- routes/pageEntreprise.js
+++ routes/pageEntreprise.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { creatPageEntreprise, addNewAdmin, getPageEntreprise } = require('../controllers/pageEntreprise')
+const { creatPageEntreprise, addNewAdmin, getPageEntreprise, removePageEntreprise, removeAdmin } = require('../controllers/pageEntreprise')
 const checkAuth = require('../middlewares/check-auth')
-const checkUser = require('../middlewares/check-user')
 const router = express.Router()
 
 router.get('/:id', getPageEntreprise)
 router.post('/', checkAuth,creatPageEntreprise)
 router.put('/:id',checkAuth,addNewAdmin)
+router.delete('/:id',checkAuth,removePageEntreprise)
+router.delete('/:id',checkAuth, removeAdmin)
  module.exports = router
