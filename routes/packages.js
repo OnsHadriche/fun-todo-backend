b81ea9b32367a00a6a1ef4986ee1ea9be075cc4a
Ons Hadriche <ounshadriche@gmail.com>
2022-04-17 16:27:20 +0200

    Add check-auth to protect the rootes

--- routes/packages.js
+++ routes/packages.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { getAllPackages,getPackages, getOnePackage, createPackage, updatePackage } = require("../controllers/packages")
+const { getAllPackages,getPackages, getOnePackage, createPackage, updatePackage,deletePackage } = require("../controllers/packages")
+const checkAuth = require('../middlewares/check-auth')
 
 
 router.get('/', getAllPackages)
 router.get('/country', getPackages)
-router.get('/:id', getOnePackage)
-router.post('/', createPackage)
-router.put('/:id', updatePackage)
+router.get('/:id', checkAuth, getOnePackage)
+router.post('/create-package',checkAuth, createPackage)
+router.put('/:id',checkAuth, updatePackage)
+router.delete('/:id',checkAuth, deletePackage)
 
 module.exports = router
 
