45d03f44705b63e4e15cfbd5aa6766f69a25f6dd
Ons Hadriche <ounshadriche@gmail.com>
2022-04-21 02:21:35 +0200

    Add new key to package mode

--- models/Package.js
+++ models/Package.js
@@ -1,30 +1,35 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const packageSchema = mongoose.Schema({
-    photo: {
-        type: String,
-        default:' '
-    },
-    title:{
-        type:String,
-        required: true
-    },
-    price:{
-        type: Number,
-        required:true
-    },
-    country:{
-        type:String,
-        required: true
-    },
-    details:{
-        type:String
-    },
-    page :{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "PageEntreprise"
-    }
-})
+  photo: {
+    type: String,
+    default: " ",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  details: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiredAt:{
+    type:Date,
+    required: true
+  },
+  page: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PageEntreprise",
+  },
+});
 
-module.exports = mongoose.model('Package', packageSchema)
+module.exports = mongoose.model("Package", packageSchema);
