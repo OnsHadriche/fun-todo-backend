2d4b099c400710a2d9c1f5b843fdb31d6d2fb140
Ons Hadriche <ounshadriche@gmail.com>
2022-06-23 12:45:57 +0200

    Add ne route to get data

--- routes/packages.js
+++ routes/packages.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getAllPackages,getPackage, getOnePackage, createPackage, updatePackage,deletePackage, getPackagePrice } = require("../controllers/packages")
+const { getAllPackages,getPackage, getOnePackage, createPackage, updatePackage,deletePackage, getPackagePrice, getPackageByPage } = require("../controllers/packages")
 const checkAuth = require('../middlewares/check-auth')
 
 
@@ -8,6 +8,7 @@ router.get('/', getAllPackages)
 router.get('/country', getPackage)
 router.get('/price', getPackagePrice)
 router.get('/:id', checkAuth, getOnePackage)
+router.get('/pack-created-page/:id',checkAuth, getPackageByPage)
 router.post('/create-package/:pageId',checkAuth, createPackage)
 router.put('/:id',checkAuth, updatePackage)
 router.delete('/:id',checkAuth, deletePackage)
