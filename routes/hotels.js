fcd8a9c4fa990a70a3f7d136084bfe569aba1f82
Ons Hadriche <ounshadriche@gmail.com>
2023-02-13 22:17:17 +0100

    Set up routes

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getAllHotels,getHotel, getOneHotel, createHotel, getHotelPrice, updateHotel, deleteHotel, getHotelByAgency, addRatingsHotel, claculateAverageRating,  } = require("../controllers/hotels")
 const checkAuth = require('../middlewares/check-auth')
+const multerFileUploadCloud =require("../middlewares/multer-fileUpload/cloudinary")
 
 
 router.get('/', getAllHotels)
@@ -9,8 +10,8 @@ router.get('/', getAllHotels)
 router.get('/price', getHotelPrice)
 router.get('/:id', checkAuth, getOneHotel)
 router.get('/hotel-by-page/:id', checkAuth,getHotelByAgency)
-router.post('/create-hotel/:pageId',checkAuth, createHotel)
-router.put('/:id',checkAuth, updateHotel)
+router.post('/create-hotel/:pageId',multerFileUploadCloud,checkAuth, createHotel)
+router.put('/:id',checkAuth,multerFileUploadCloud,updateHotel)
 router.delete('/:id',checkAuth, deleteHotel)
 router.put('/add-rating-hotel/:id', checkAuth, addRatingsHotel);
 router.get('/rating/average-rate',claculateAverageRating)
--- routes/packages.js
+++ routes/packages.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getAllPackages,getPackage, getOnePackage, createPackage, updatePackage,deletePackage, getPackagePrice, getPackageByPage } = require("../controllers/packages")
 const checkAuth = require('../middlewares/check-auth')
+const multerFileUploadCloud =require("../middlewares/multer-fileUpload/cloudinary")
 
 
 router.get('/', getAllPackages)
@@ -9,8 +10,8 @@ router.get('/country', getPackage)
 router.get('/price', getPackagePrice)
 router.get('/:id', checkAuth, getOnePackage)
 router.get('/pack-created-page/:id',checkAuth, getPackageByPage)
-router.post('/create-package/:pageId',checkAuth, createPackage)
-router.put('/:id',checkAuth, updatePackage)
+router.post('/create-package/:pageId',multerFileUploadCloud, checkAuth, createPackage)
+router.put('/:id',checkAuth,multerFileUploadCloud, updatePackage)
 router.delete('/:id',checkAuth, deletePackage)
 
 module.exports = router
